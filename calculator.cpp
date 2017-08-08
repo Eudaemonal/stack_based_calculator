5c36d4b8ffa5d3aa44b74aae660ce8e5b1171157
eudaemonal <eudaemonal@gmail.com>
2017-08-09 09:53:35 +1000

    test passed

--- calculator.cpp
+++ calculator.cpp
@@ -13,20 +13,17 @@
 const char* operators[] = {"add", "sub", "mult", "div", "sqrt", "pop", "reverse", "repeat", "endrepeat"};
 std::vector<std::string> oplib(operators, std::end(operators));
 
+// operator and operand stack
 std::deque<double> operandStack;
-std::deque<bool> dtypeStack;
+std::deque<bool> dtypeStack; // digit type for int or double
 std::deque<std::string> operatorStack;
 
-std::deque<std::string> tokenStack; // tokenStack for repeat
+ // tokenStack for processing and repeat
+std::deque<std::string> tokenStack;
 std::deque<std::string> tokenRepStack;
 
 
-std::deque<double> operandRepStack;
-std::deque<bool> dtypeRepStack;
-std::deque<std::string> operatorRepStack;
-
-bool inRepeat = false;
-bool runRepeat = false;
+int inRepeat = 0;
 int repeatTimes = 0;
 
 // if a token is a operator
@@ -54,10 +51,61 @@ bool isint(std::string s){
 }
 
 
-void calculate(){
+// basic display
+void basicdisplay(char opt){
+	bool a, b, c;
 	double x, y, z;
+	a = dtypeStack.back();
+	dtypeStack.pop_back();
+	b = dtypeStack.back();
+	dtypeStack.pop_back();
+	c = a||b;
+
+	x = operandStack.back();
+	operandStack.pop_back();
+	y = operandStack.back();
+	operandStack.pop_back();
+
+	if(opt=='+')
+		z = x + y;
+	else if(opt=='-')
+		z = x - y;
+	else if(opt=='*')
+		z = x * y;
+	else if(opt=='/')
+		z = x / y;
+
+	if(!a)
+		std::cout << (int)x;
+	else
+		std::cout << x;
+	std::cout <<" "<<opt<<" ";
+
+	if(!b)
+		std::cout << (int)y;
+	else
+		std::cout << y;
+
+	std::cout <<" = ";
+
+	if(!c)
+		std::cout << (int)z;
+	else
+		std::cout << z;
+	std::cout << std::endl;
+	
+	operandStack.push_back(z);
+	dtypeStack.push_back(c);
+
+}
+
+
+void calculate(){
+	double x, z;
        	int i, n = 0;	// numbers of reverse
+	bool a;
 	std::deque<double> reverseQueue;
+	std::deque<bool> reverseTypeQueue;
 
 	std::string s = operatorStack.back();
 	operatorStack.pop_back();
@@ -72,83 +120,70 @@ void calculate(){
 	std::cout<< "=============OP Stack===========" <<std::endl;
 	*/
 		if(s =="add"){
-			x = operandStack.back();
-			operandStack.pop_back();
-			y = operandStack.back();
-			operandStack.pop_back();
-			z = x + y;
-			std::cout <<x << " + " << y << " = "<< z <<std::endl;
-			operandStack.push_back(z);
+			basicdisplay('+');
 		}
 		else if(s=="sub"){
-			x = operandStack.back();
-			operandStack.pop_back();
-			y = operandStack.back();
-			operandStack.pop_back();
-			z = x - y;
-			std::cout <<x << " - " << y << " = "<< z <<std::endl;
-			operandStack.push_back(z);
+			basicdisplay('-');
 		}
 		else if(s=="mult"){
-			x = operandStack.back();
-			operandStack.pop_back();
-			y = operandStack.back();
-			operandStack.pop_back();
-			z = x * y;
-			std::cout <<x << " * " << y << " = "<< z <<std::endl;
-			operandStack.push_back(z);	
+			basicdisplay('*');
 		}
 		else if(s=="div"){
-			x = operandStack.back();
-			operandStack.pop_back();
-			y = operandStack.back();
-			operandStack.pop_back();
-			z = x / y;
-			std::cout <<x << " / " << y << " = "<< z <<std::endl;
-			operandStack.push_back(z);	
+			basicdisplay('/');
+
 		}
 		else if(s=="sqrt"){
 			x = operandStack.back();
 			operandStack.pop_back();
+			a = dtypeStack.back();
+			dtypeStack.pop_back();
+
 			z = sqrt(x) ;
 			std::cout <<"sqrt " << x << " = "<< z <<std::endl;
-			operandStack.push_back(z);		
+			operandStack.push_back(z);
+			dtypeStack.push_back(a);		
 		}
 		else if(s=="pop"){
 			operandStack.pop_back();
+			dtypeStack.pop_back();
 		}
 		else if(s=="reverse"){
 			n = (int)operandStack.back();
 			operandStack.pop_back();
+			dtypeStack.pop_back();
 			for(i=0;i<n;++i){
 				x = operandStack.back();
 				operandStack.pop_back();
 				reverseQueue.push_front(x);
+				a = dtypeStack.back();
+				dtypeStack.pop_back();
+				reverseTypeQueue.push_front(a);
+
+				
 			}
 			for(i=0;i<n;++i){
 				x = reverseQueue.back();
 				reverseQueue.pop_back();
 				operandStack.push_back(x);
+				a = reverseTypeQueue.back();
+				reverseTypeQueue.pop_back();
+				dtypeStack.push_back(a);
 			}
 		}
-
-		
-		
-		if(s=="repeat"){
-			// Handling nested repeat
-			if(inRepeat == false){
+		else if(s=="repeat"){
 			repeatTimes = (int)operandStack.back();
 			operandStack.pop_back();
+			dtypeStack.pop_back();
 
 			//std::cout<< "Repeat Times: "<<repeatTimes <<std::endl;//test
 			//std::cout<< "Next: "<<operandStack.back() <<std::endl;//test
 			
-			inRepeat = true;
-			}
+			inRepeat++;
 		}
 		else if(s=="endrepeat"){
-			inRepeat = false;
-
+			if(inRepeat > 0)
+				inRepeat --;
+			if(inRepeat==0){
 			for(i=0;i<repeatTimes-1;++i){
 				for(n=tokenRepStack.size()-1;n>=0;n--){
 				tokenStack.push_back(tokenRepStack.at(n));
@@ -156,31 +191,8 @@ void calculate(){
 				}
 			}
 			tokenRepStack.clear();
-
-			/*
-			x = operandStack.back();
-			operandStack.pop_back();
-			
-			for(i=0;i<repeatTimes-1;++i){
-				for(n=0;n<operandRepStack.size();++n){
-				operandStack.push_back(operandRepStack.at(n));
-				//std::cout<< "Push:" << operandRepStack.at(n) <<std::endl; //test
-				}
-				for(n=0;n<operatorRepStack.size();++n){
-				operatorStack.push_back(operatorRepStack.at(n));
-				std::cout<< "Push:" << operatorRepStack.at(n) <<std::endl; //test
-				}
 			}
-			operandStack.push_back(x);
-			
-			operandRepStack.clear();
-			dtypeRepStack.clear();
-			operatorRepStack.clear();
-			*/
-			
 		}
-		
-		
 
 }
 
@@ -189,14 +201,31 @@ void calculate(){
 void process(std::string s){
 	bool dtype = 0; // int: 0 ;double: 1
 
+
 	if(isdigit(s)){
 		if(isint(s))
 			dtype = 0;
 		else
 			dtype = 1;
 
+	/*
+	std::cout<< "=============OP Stack===========" <<std::endl;	
+	std::cout<< "size: "<< operandStack.size()<< "  size: "<< dtypeStack.size()<<std::endl;
+	for(int i=0;i<operandStack.size();i++){
+	std::cout<< " " << operandStack.at(i);
+	}
+	std::cout << std::endl;
+	for(int i=0;i<dtypeStack.size();i++){
+	std::cout<< " " << dtypeStack.at(i);
+	}
+	std::cout << std::endl;
+	std::cout<< "-------------OP Stack-----------" <<std::endl;
+	*/
+
 		operandStack.push_back(std::stod(s));
 		dtypeStack.push_back(dtype);
+
+
 	}
 	else if(isoperator(s)){
 		operatorStack.push_back(s);
@@ -226,7 +255,7 @@ int main(int argc, char* argv[]) {
 	// read the file while we have input.
 	while (in >> s) {
 		tokenStack.push_front(s);
-		if(inRepeat && s!="endrepeat"){
+		if(inRepeat > 0 && (s!="endrepeat" && inRepeat ==1)){
 			tokenRepStack.push_front(s);
 		}
 		
@@ -237,7 +266,6 @@ int main(int argc, char* argv[]) {
 		process(s);
 
 		}
-
 	}
 	in.close();
 	
